7c05418e501950edea7843107429e7856309ca3b
RajaMaruthi <rekhaadbn@gmail.com>
2023-07-03 16:09:00 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+		System.out.println("Hello Maruthi");
+		System.out.println("Hello Aadhya");
+		System.out.println("Hello Aaa");
+		System.out.println("Hello A");
+		
+		System.out.println("Hello B");
+		System.out.println("Hello Aff");
+		
+		int a=10;
+		
+		int b=20;
+		
+		float b-10.0;
+    }
+
+}
